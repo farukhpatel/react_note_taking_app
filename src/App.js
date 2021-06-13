3ff45fd7982fc1d75eb84890f25d7cf3d252e4bc
farukhpatel <farukhpatel0804@gmail.com>
2021-06-13 14:15:20 +0530

    useContext hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { log } from 'async';
+import React,{useContext, useState,useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { myContext } from './Context';
+
+const App=()=>{
+
+  const dataFromContext=useContext(myContext);
+  const [data,setData]=useState(dataFromContext);
+  useEffect(()=>{
+    console.log("useEffect work component mount");
+  },[]);  //[] this is dependency object
+  //[] In this object you can give state that means when state is changes
+  //useEffect also called
+  const someWork=(e)=>{
+    e.preventDefault();
+    console.log("your some work");
+    console.log(e);
+  }
+  return(
+    <div>
+      <h1>hello, {data.name}</h1>
+      {
+        // event onClick,onSubmit,onChange
+      }
+      <button onClick={(e)=>{someWork(e)}}>Click</button>
     </div>
   );
 }
-
 export default App;
